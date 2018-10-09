85cf88467a3c35db8aa1a6a92d25469c1b6d4527
sergioguaka <scm0@correo.ugr.es>
2018-10-09 19:25:41 +0200

    "First try"

---
+++ II_Ordenados_Ampliacion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+enum Valor {A, B, C};
+
+int main () {
+   // Entrada de datos
+   cout << "Introduce 3 números enteros (a b c): ";
+   Valor::A
+
+   // Cálculos
+   cout << "holi";
+}
