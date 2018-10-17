930089ed884c54f7bce075f08e13fbaba8bd7f5b
Sergio Campoy <43934965+sergioguaka@users.noreply.github.com>
2018-10-17 19:00:12 +0200

    Ejercicio 42

---
+++ fp_sesion06/II_Bisiesto_Ciclos.cpp
@@ -0,0 +1,41 @@
+/*
+Sergio Campoy Maldonado, Fundamentos de Programación 1ºC
+
+Programa que devuelve los años bisiestos entre dos años dados
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	//	Declaración de constantes
+	const int Year_min = 1900;
+	
+	//	Declaración de variables
+	int year1, year2;
+	bool multi4, multi100, multi400;
+	
+	//	Entrada de datos
+	do {
+		cout << "Introduce el anyo inicial: ";
+		cin >> year1;
+	} while (year1 < Year_min);
+	
+	do {
+		cout << "Introduce el anyo final: ";
+		cin >> year2;
+	} while (year2 < Year_min);
+	
+	//	Cálculos
+	int i = year1;	//	Variable contador
+	while (i <= year2) {
+		multi4 = (i % 4 == 0);
+		multi100 = (i % 100 == 0);
+		multi400 = (i % 400 == 0);
+		
+		if ((multi4 && !multi100) || (multi400))
+			cout << i << " es bisiesto" << endl;
+		i++;
+	}
+}
