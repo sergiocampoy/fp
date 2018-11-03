1f735298d6b8c15e5b802b2e107c0a9c9bc11e71
sergioguaka <scm0@gmail.com>
2018-11-03 22:48:47 +0100

    Ejercicios

---
+++ fp_sesion08/IV_Circunferencia_Regs.cpp
@@ -0,0 +1,154 @@
+/*
+Sergio Campoy Maldonado
+Fundamentos de Programación
+1ºC
+
+Programa que:
+	1)	Recibe el centro y el radio de una circunferencia y calcula la longitud de
+		esta y el área del círculo.
+	2)	Recibe una lista de puntos y comprueba si la circunferencia contiene a
+		cada punto.
+*/
+
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+//	Declaración de constantes
+const double PI = 3.14159265358979323846;
+const string TERMINADOR = "FIN";
+
+struct Punto2D {
+	double x, y;		//	Abscisa y ordenada del punto
+};
+
+struct Circunferencia {
+	Punto2D centro;	//	Centro de la circunferencia (x, y)
+	double radio;		//	Radio de la circunferencia
+};
+
+/******************************************************************************/
+//	Función:		Lee_Numero
+//	Recibe:		void
+//	Devuelve:	Un double leído con getline (cin, n) para evitar problemas con el
+//					buffer del teclado
+/******************************************************************************/
+
+double Lee_Numero () {
+	string n;
+	getline (cin, n);
+	
+	return (stod (n));
+}
+
+/******************************************************************************/
+//	Función:		Cuadrado
+//	Recibe:		"x" un double
+//	Devuelve:	"x^2", otro double
+/******************************************************************************/
+
+double Cuadrado (double x) {
+	return (x*x);
+}
+
+/******************************************************************************/
+//	Función:		Longitud
+//	Recibe:		"circ" del tipo Circunferencia
+//	Devuelve:	Longitud de la circunferencia (double)
+/******************************************************************************/
+
+double Longitud (Circunferencia circ) {
+	return (2 * PI * circ.radio);
+}
+
+/******************************************************************************/
+//	Función:		Area
+//	Recibe:		"circ" del tipo Circunferencia
+//	Devuelve:	Área del círculo (double)
+/******************************************************************************/
+
+double Area (Circunferencia circ) {
+	return (PI * circ.radio * circ.radio);
+}
+
+/******************************************************************************/
+//	Función:		Contiene_Punto
+//	Recibe:		"p" Punto2D
+//					"circ" Circunferencia
+//	Devuelve:	"TRUE" si la circunferencia contiene a "p"
+//					"FALSE" si la circunferencia NO contiene a "p"
+/******************************************************************************/
+
+bool Contiene_Punto (Punto2D p, Circunferencia circ) {
+	double distancia_cuadrado;
+	
+	distancia_cuadrado = Cuadrado (circ.centro.x - p.x)
+							 + Cuadrado (circ.centro.y - p.y);
+	
+	return (distancia_cuadrado <= Cuadrado (circ.radio));
+}
+
+int main (void) {
+	//	Declaración de variables
+	Circunferencia circ;
+	
+	//	Entrada
+	cout << "Introduce el centro de la circunferencia (x, y)" << endl;
+	cout << "\tx: ";
+	circ.centro.x = Lee_Numero ();
+	cout << "\ty: ";
+	circ.centro.y = Lee_Numero ();
+	
+	cout << "Introduce el radio de la circunferencia: ";
+	circ.radio = Lee_Numero ();
+	
+	//	Salida
+	cout << "La longitud de la circunferencia es " << Longitud (circ) << endl;
+	cout << "El area del circulo es " << Area (circ) << endl;
+	
+	
+/******************************************************************************/
+/*Segundo apartado*/
+/******************************************************************************/	
+	
+	//	Declaración de variables
+	Punto2D p;
+	string cad;
+	
+	cout << endl
+		  << "Introduce un punto para comprobar si esta dentro del circulo"
+		  << " (o \"FIN\" para salir)"
+		  << endl;
+	
+	//	Lectura adelantada
+	cout << "Punto: (x, y)" << endl;
+	
+	cout << "\tx: ";
+	getline (cin, cad);
+	
+	while (cad != TERMINADOR) {
+		p.x = stod (cad);
+		
+		cout << "\ty: ";
+		p.y = Lee_Numero ();
+		
+		if (Contiene_Punto (p, circ))
+			cout << "El punto (" << p.x << ", " << p.y << ")"
+				  << " SI pertenece a la circunferencia"
+				  << endl;
+		else
+			cout << "El punto (" << p.x << ", " << p.y << ")"
+				  << " NO pertenece a la circunferencia"
+				  << endl;
+		
+		//	Lectura
+		cout << "Punto: (x, y)" << endl;
+	
+		cout << "\tx: ";
+		getline (cin, cad);
+	}
+
+	return 0;
+}
---
+++ fp_sesion08/IV_GaussianaCDF_Regs.cpp
@@ -0,0 +1,131 @@
+/*
+Sergio Campoy Maldonado
+Fundamentos de Programación
+1ºC
+
+
+*/
+
+#include <iostream>
+
+#include <cmath>
+
+#include <iomanip>
+
+#include <string>
+
+using namespace std;
+
+//	Declaración de constantes globales
+const double PI = 3.14159265358979323846;
+
+
+struct Gaussiana {
+	double x, media, desv;	//	Valor de x, media / esperanza y desviación típica
+};
+
+/******************************************************************************/
+//	Función:		Valor_x
+//	Recibe:		"campana" del tipo Gaussiana
+//	Devuelve:	Imagen de la función en el punto x (double)
+/******************************************************************************/
+
+double Valor_x (Gaussiana campana) {
+	double base, exponente;
+	
+	base = 1 / (campana.desv * sqrt (2*PI));
+	exponente = pow ((campana.x - campana.media) / campana.desv, 2) / (-2);
+	
+	return (base * exp (exponente));
+}
+
+/******************************************************************************/
+//	Función: 	Funcion_CDF
+//	Recibe:		"campana" del tipo Gaussiana
+//	Devuelve:	Área de la función hasta el valor de x (double)
+
+//	PRE:			campana.media = 0		(Distribución normal estandar)
+//					campana.desv = 1
+/******************************************************************************/
+
+double Funcion_CDF (Gaussiana campana) {
+	
+	//	Declaración de constantes
+	const double b0 = 0.2316419, 	 b1 = 0.319381530,  b2 = -0.356563782,
+					 b3 = 1.781477937, b4 = -1.821255978, b5 = 1.330274429;
+	
+	//	Declaración de variables
+	double w, t = 1.0 / (1 + b0 * abs (campana.x));
+	
+	//	Cálculos
+	w = 1 - Valor_x (campana) *
+		(b1*t + b2*pow(t, 2) + b3*pow(t, 3) + b4*pow(t, 4) + b5*pow(t, 5));
+	
+	if (campana.x < 0)
+		w = (1 - w);
+	
+	//	Return
+	return w;
+}
+
+int main () {
+	cout.setf(ios::fixed);		// Notación de punto fijo para los reales
+	cout.setf(ios::showpoint);	// Mostrar siempre decimales
+	cout << setprecision (4);	//	Precisión de los decimales
+	
+	//	Declaración de variables
+	Gaussiana gauss;
+	bool salir = false, estandar;
+	double lim_inf, lim_sup;
+	string respuesta;
+	
+	while (!salir) {
+		//	Entrada
+		cout << "Introduce los parametros de la campana de Gauss: " << endl;
+		cout << "\tMedia: ";
+		cin >> gauss.media;
+		
+		do {
+			cout << "\tDesviacion tipica: ";
+			cin >> gauss.desv;
+		} while (gauss.desv <= 0);
+		
+		//	Cálculos
+		lim_inf = gauss.media - 3 * gauss.desv;
+		lim_sup = gauss.media + 3 * gauss.desv;
+		
+		estandar = ((gauss.desv == 1) && (gauss.media == 0));
+		
+		//	Salida
+		cout << "Los valores de la campana son: " << endl;
+		
+		for (gauss.x = lim_inf; gauss.x <= lim_sup; gauss.x += gauss.desv/2) {
+			cout << "\tx = " << gauss.x;
+			cout << "\tg (x) = " << Valor_x (gauss);
+			
+			if (estandar)
+				cout << "\t\tCDF (x) = " << Funcion_CDF (gauss);
+			
+			cout << endl;
+		}
+		
+		if (!estandar)
+			cout << endl
+				  << "No se trata de la distribucion normal estandar," << endl
+				  << "por lo que no se puede aplicar la aproximacion "
+				  << "de Abramowitz y Stegun"
+				  << endl << endl;
+		
+		do {
+			cout << "Desea continuar? (SI / NO): ";
+			cin >> respuesta;
+		} while ((respuesta != "SI") && (respuesta != "NO"));
+		
+		if (respuesta == "NO")
+			salir = true;
+		
+		cout << endl;
+	}
+	
+	return 0;
+}
