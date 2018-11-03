7911ed759d071886c66bbb0222ad26ab5cf8680e
sergioguaka <scm0@gmail.com>
2018-11-03 12:34:18 +0100

    Ejercicio 2 (WIP)

---
+++ fp_sesion08/IV_Circunferencia_Regs_beta.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//	Declaración de constantes
+const double PI = 3.14159265358979323846;
+
+struct Punto2D {
+	double x, y;		//	Abscisa y ordenada del punto
+};
+
+struct Circunferencia {
+	Punto2D centro;
+	double radio;
+};
+
+
+
+int main (void) {
+	
+}
